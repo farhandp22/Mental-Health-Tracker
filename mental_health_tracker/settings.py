7c6efe104c9618602799568b6c866ba08123d5d5
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-09-11 16:44:39 +0700

    pbp

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://farhan-dwi-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://farhan-dwi-mentalhealthtracker2.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://farhan-dwi-mentalhealthtracker3.pbp.cs.ui.ac.id", "https://farhan-dwi-mentalhealthtracker3.pbp.cs.ui.ac.id"]
