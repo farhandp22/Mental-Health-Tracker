ba2adf1abdc8ed84caeffc3f83e788cd5007daaf
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-10-02 16:08:11 +0700

    tutorial 5

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
@@ -12,6 +14,7 @@ from django.shortcuts import render, redirect,reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.contrib.auth import logout
+from django.utils.html import strip_tags
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
@@ -40,11 +43,11 @@ def show_main(request):
     return render(request, "main.html", context)
 # Create your views here.
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -70,16 +73,19 @@ def login_user(request):
    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
 
+
       if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-
+      else:
+            messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
-   context = {'form': form}
+      context = {'form': form}
+     
    return render(request, 'login.html', context)
 
 def logout_user(request):
@@ -110,3 +116,20 @@ def delete_mood(request, id):
     mood.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) 
+    feelings = strip_tags(request.POST.get("feelings"))
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
